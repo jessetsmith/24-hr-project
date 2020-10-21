ddc57b8e2a6edf53c7eb2ae9974a6ebc74354dbb
Jesse <cappellacoding@gmail.com>
2020-10-21 18:08:57 -0400

    First Commit

---
+++ 24HourProject.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _24HourProject.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ 24HourProject.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24HourProject.WebAPI.WebApiApplication" Language="C#" %>
