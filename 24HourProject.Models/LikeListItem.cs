b772ae7ee80521b087191389a4ec68195afa0e5d
Nathena S Dodd <69280713+Nathenadodd@users.noreply.github.com>
2020-10-22 22:15:28 -0400

    like class, services,controllers

--- 24HourProject.Models/LikeListItem.cs
+++ 24HourProject.Models/LikeListItem.cs
@@ -1,23 +1,18 @@
-﻿using System;
+﻿using _24HourProject.Data;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _24HourProject.Data
+namespace _24HourProject.Models
 {
-    public class Like
+   public class LikeListItem
     {
-        [Required]
         [ForeignKey(nameof(Post))]
         public Post LikedPost { get; set; }
-        [Required]
         [ForeignKey(nameof(User))]
-        public Guid OwnerId { get; set; }
         public User Liker { get; set; }
-        [Required]
-        public int LikeId { get; set; }
     }
 }
